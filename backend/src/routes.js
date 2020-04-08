7223406d49eecb7a082fb16246cfb3113a3d20ed
Vitor716 <62679174+Vitor716@users.noreply.github.com>
2020-04-08 19:41:07 -0300

    Front-end

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,16 +6,49 @@ const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
+const { celebrate,Segments, Joi} = require('celebrate')
 
 routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs' , OngController.index);
-routes.post('/ongs' , OngController.create);
 
-routes.get('/profile',ProfileController.index);
 
-routes.get('/incidents' ,IncidentController.index);
-routes.post('/incidents' ,IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.post('/ongs' ,celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(8).max(9),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), OngController.create);
+
+
+routes.get('/profile',celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(), 
+    }).unknown(),
+}),ProfileController.index);
+
+routes.get('/incidents' ,celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    })
+}), IncidentController.index);
+
+routes.post('/incidents',celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number().required(),
+    })
+}),IncidentController.create);
+
+
+routes.delete('/incidents/:id',celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required(),
+    })
+}), IncidentController.delete);
 
 module.exports = routes;
---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,3 @@
+const request = require('express');
+const app = require ('../../src/app');
+const connection  = require ('../../src/database/connection');
